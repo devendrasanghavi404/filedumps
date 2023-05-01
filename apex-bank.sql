ea38e6e9972655495c21c53bfec2c41ef43ca65f
Devendra_Sanghavi <115162122+devendrasanghavi404@users.noreply.github.com>
2023-05-01 15:12:29 +0530

    Add files via upload

---
+++ apex-bank.sql
@@ -0,0 +1,101 @@
+DROP DATABASE IF EXISTS demo_bank_v1;
+
+CREATE DATABASE demo_bank_v1;
+-- USERS TABLE STRUCTURE;
+
+USE demo_bank_v1;
+
+CREATE TABLE users(
+	user_id INT NOT NULL auto_increment Primary Key,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NULL
+);
+
+-- BANK ACCOUNTS TABLE STRUCTURE;
+CREATE TABLE accounts(
+	account_id INT NOT NULL auto_increment primary key,
+    user_id INT,
+    account_number VARCHAR(100) NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
+    account_type VARCHAR(50) NOT NULL,
+    balance DECIMAL(18, 2) DEFAULT 0.00,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NOW(),
+    foreign key(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- TRANSACTION HISTORY TABLE;
+CREATE TABLE transaction_history(
+	transaction_id INT NOT NULL auto_increment Primary key,
+    account_id INT,
+    transaction_type VARCHAR(50) NOT NULL,
+    amount DECIMAL(18, 2),
+    source VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL, -- success/failed
+    reason_code VARCHAR(100) NULL,
+    created_at TIMESTAMP,
+    FOREIGN KEY(account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+);
+
+-- PAYMENTS TABLE STRUCTURE
+CREATE TABLE payments(
+	payment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id INT,
+    beneficiary VARCHAR(50) NULL,
+    beneficiary_acc_no VARCHAR(255) NULL,
+    amount DECIMAL(18, 2) NULL,
+    reference_no VARCHAR(100) NULL,
+    status VARCHAR(50) NOT NULL, -- success/failed
+    reason_code VARCHAR(100) NULL,
+    created_at TIMESTAMP,
+    FOREIGN KEY(account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+);
+
+-- TRANSACTION HISTORY VIEW;
+
+SELECT 
+	t.transaction_id,
+	a.account_id,
+	u.user_id,
+	t.transaction_type,
+	t.amount,
+	t.source,
+	t.status,
+	t.reason_code,
+	t.created_at
+FROM
+	transaction_history AS t
+INNER JOIN
+	accounts AS a
+ON
+	t.account_id = a.account_id
+INNER JOIN
+	users as u
+ON
+	a.user_id = u.user_id;
+    
+    
+-- PAYMENT HISTORY VIEW;
+Select
+	p.payment_id,
+    a.account_id,
+    u.user_id,
+    p.beneficiary,
+    p.beneficiary_acc_no,
+    p.amount,
+    p.status,
+    p.reference_no,
+    p.reason_code,
+    p.created_at
+FROM
+	payments AS p
+INNER JOIN
+	accounts AS a
+ON
+	p.account_id=a.account_id
+INNER JOIN
+	users AS u
+ON
+	a.user_id = u.user_id
